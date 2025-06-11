3199154d7e83edf4e857bfa9e901cdbc3e3803d9
aguerrero <aguerrero@windowschannel.com>
2025-06-11 15:14:35 -0500

    minor change

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { AppModule } from './app.module';
 
 import { SocketIoAdapter } from './socket-io-adapter';
 
-// import { SocketIoAdapter } from './socket-io-adapter';
-
 async function bootstrap() {
   process.env.TZ = 'America/Bogota';
   
@@ -13,28 +11,29 @@ async function bootstrap() {
     const corsOptions = {
     origin: [
       'http://localhost:5173',
-      'http://localhost:3000',
-      'http://devel1.klmsystem.test:5173',
-      'http://devel1.klmsystem.test',
+      'http://devel.klmsystem.test:5173',
+      'http://devel.klmsystem.test:3000',
       'https://klmsystem.online'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   };
+
   app.useWebSocketAdapter(new SocketIoAdapter(app, corsOptions));
   app.setGlobalPrefix('v1');
 
     app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,  // elimina propiedades no definidas en el DTO
-    forbidNonWhitelisted: true,  // error si llegan propiedades extra
-    transform: true,  // transforma payload a instancias de clases DTO
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
   }));
   // Configurar CORS
   app.enableCors({
     origin: [
-      'http://localhost:3000', // Por si usas otro puerto para testing
-      'http://devel1.klmsystem.test:5173', // Tu dominio personalizado
-      'http://devel1.klmsystem.test', // Sin puerto también
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://devel.klmsystem.test:5173',
+      'http://devel.klmsystem.test:3000', 
       'https://klmsystem.online'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
